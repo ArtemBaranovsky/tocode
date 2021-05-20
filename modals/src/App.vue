726c95d95ba6878b5e27440dbbd47fbdf1c897e9
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2021-05-20 14:20:03 +0300

    My modals app's initial commit

---
+++ modals/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+
+      <section>
+        <div class="container">
+          <button class="btn btnPrimary" @click="modalFirst = ! modalFirst">Show first modal</button>
+
+          <!-- First modal -->
+          <modals
+              title="First modal"
+              v-if="modalFirst"
+          ></modals>
+        </div>
+      </section>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import modals from '@/components/Modal'
+
+export default {
+  name: 'My modals',
+  components: {
+    modals
+  },
+  data() {
+    return {
+      modalFirst: false
+    }
+  }
+}
+</script>
