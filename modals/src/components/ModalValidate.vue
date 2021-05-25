7533e460ca4b2aac0805194345861b5098740dee
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2021-05-25 17:09:17 +0300

    Added onSubmit method to submit form.

--- modals/src/components/ModalValidate.vue
+++ modals/src/components/ModalValidate.vue
@@ -4,7 +4,7 @@
         @close="$emit('close')">
         <!-- body-->
         <div slot="body">
-            <form @submit.prevent="">
+            <form @submit.prevent="onSubmit">
                 <!--name-->
                 <div class="form-item" :class="{ errorInput: $v.name.$error }">
                     <label>Name:</label>
@@ -50,6 +50,19 @@ export default {
   	return {
       name: '',
       email: ''
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.$v.$touch()
+
+      if (! this.$v.$invalid) {
+      	const user = {
+          name: this.name,
+          email: this.email
+        }
+        console.log(user);
+      }
     }
   },
   validations: {
