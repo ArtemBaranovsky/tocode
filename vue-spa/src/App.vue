bcf65e55fdeee09aeef86f319fac96e6db4262be
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2021-06-10 15:47:53 +0300

    Added dynamic links.

--- vue-spa/src/App.vue
+++ vue-spa/src/App.vue
@@ -7,11 +7,8 @@
            <div class="navbar-content">
              <div class="logo">VUE-CLI</div>
              <ul class="navbar-list">
-               <li class="navbar-item">
-                 <a href="/" class="navbar-link">Home</a>
-               </li>
-               <li class="navbar-item">
-                 <a href="/example" class="navbar-link">Example</a>
+               <li class="navbar-item" v-for="link in links" :key="link.title">
+                 <router-link :title="link.title" :to="link.url" class="navbar-link">{{ link.title }}</router-link>
                </li>
              </ul>
            </div>
@@ -27,5 +24,14 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      links: [
+        { title: 'Home', url: '/'},
+        { title: 'Example', url: '/example'}
+      ]
+    }
+  }
+}
 </script>
