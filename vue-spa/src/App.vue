77ac76a0598085d691d4d41a69e5b280030e208e
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2021-06-10 14:29:03 +0300

    Vue-spa installation and setting 2 base routes.

---
+++ vue-spa/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+
+     <header>
+       <h1>Header</h1>
+     </header>
+
+    <router-view></router-view>
+
+    <!-- <footer></footer> -->
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
