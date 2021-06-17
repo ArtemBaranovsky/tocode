7cbd149ba758a06915d96dff146f9ed08fe52177
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2021-06-17 17:56:57 +0300

    Babel, webpack installation and setting.

---
+++ webpacktemplate/src/index.js
@@ -0,0 +1,16 @@
+
+import './js/common'
+
+
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+
+// 1-st option to use vue
+// import 'vue'
+
+// 2-st option to use vue
+// import Vue from 'vue'
+// Vue.use()
+
+// 3-st option to use vue
+window.Vue = require('vue')
