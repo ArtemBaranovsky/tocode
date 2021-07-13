a749e785d992358f77f5b7f3d1dc297a72e48da5
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2021-07-13 11:25:28 +0300

    Hardcoded tables example

--- usersCRM/src/pages/Users.vue
+++ usersCRM/src/pages/Users.vue
@@ -2,8 +2,42 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <h1>Users Page</h1>
+
+        <!--table-->
+        <table>
+
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Gender</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for="user in users" :key="user.id">
+              <td> {{ user.name }} </td>
+              <td> {{ user.age }} </td>
+              <td> {{ user.gender }} </td>
+            </tr>
+          </tbody>
+
+        </table>
+
       </div>
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      users: [  // only for example
+        { id: 1, name: 'Jack', age: 22, gender: 'male' },
+        { id: 2, name: 'Alex', age: 23, gender: 'male' }
+      ]
+    }
+  }
+}
+</script>
