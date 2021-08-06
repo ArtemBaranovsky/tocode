305719874e9a8fa6231957c7963e507b123c4757
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2021-08-06 09:14:08 +0300

    Blog project - adding a post on admin  page

---
+++ blog/components/Admin/newPostForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="new-post">
+    <div class="container">
+      <form @submit.prevent>
+        <AppInput v-model="post.title">Title:</AppInput>
+        <AppInput v-model="post.descr">Description:</AppInput>
+        <AppInput v-model="post.img">Image Link:</AppInput>
+        <AppTextArea v-model="post.content">Content:</AppTextArea>
+
+      <!--buttons-->
+      <div class="controls">
+        <AppButton class="btnDanger" @click="cancel">Cancel</AppButton>
+        <AppButton @click="onSubmit">Save</AppButton>
+      </div>
+
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "newPostForm",
+  data() {
+    return {
+      post: {
+        title: '',
+        descr: '',
+        img: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+    cancel () {
+      this.$router.push('/admin/')
+    },
+    onSubmit() {
+      this.$emit('submit', this.post)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.controls {
+  text-align: center;
+  margin: 20px 0;
+}
+</style>
---
+++ blog/pages/admin/new-post/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <new-post-form @submit="onSubmit" />
+</template>
+
+<script>
+import newPostForm from '@/components/Admin/newPostForm.vue'
+
+export default {
+  name: "index",
+  components: { newPostForm },
+  layout: 'admin',
+  methods: {
+    onSubmit(post) {
+      console.log('Post added!')
+      console.log(post);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
