3dadb4f17182f5cedff9759c12cfb58a14f67ed6
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2021-08-16 12:21:58 +0300

    Blog project - admin panel is blocked from rendering

--- blog/layouts/admin.vue
+++ blog/layouts/admin.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="wrapper">
-    <Header />
+  <no-ssr>
+    <div class="wrapper">
+      <Header />
 
-    <!--content-->
-    <div class="wrapper-content wrapper-content--fixed">
-      <Intro title="Admin page">
-        <nuxt-link to="/admin" class="link linkWhite">Admin</nuxt-link>
-        <nuxt-link to="/admin/new-post" class="link linkWhite">New Post</nuxt-link>
-        <nuxt-link to="/admin/comments" class="link linkWhite">Comments</nuxt-link>
-        <span @click="logoutUser" class="link linkWhite">Logout</span>
-      </Intro>
-      <nuxt />
+      <!--content-->
+      <div class="wrapper-content wrapper-content--fixed">
+        <Intro title="Admin page">
+          <nuxt-link to="/admin" class="link linkWhite">Admin</nuxt-link>
+          <nuxt-link to="/admin/new-post" class="link linkWhite">New Post</nuxt-link>
+          <nuxt-link to="/admin/comments" class="link linkWhite">Comments</nuxt-link>
+          <span @click="logoutUser" class="link linkWhite">Logout</span>
+        </Intro>
+        <nuxt />
+      </div>
+
     </div>
-
-  </div>
+  </no-ssr>
 </template>
 
 <script>
