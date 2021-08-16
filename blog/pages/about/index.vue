2641b7fe4a05da016d1e2aaa0fdda55c318300b6
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2021-08-16 11:49:34 +0300

    Blog project - added basic SEO tags.

--- blog/pages/about/index.vue
+++ blog/pages/about/index.vue
@@ -17,18 +17,25 @@
 </template>
 
 <script>
-export default {
-
-}
-</script>
-
-
-<script>
 import intro from '@/components/UI/Intro'
 export default {
   name: "index",
-  components : { intro }
+  components : { intro },
+  head () {
+    let title = 'About My SSR Blog!',
+      descr = 'About My SSR Blog! With Nuxt.js!',
+      type = 'site'
 
+    return {
+      title: title,
+      meta: [
+        { hid: 'og:title', name: 'og:title', content: title },
+        { hid: 'description', name: 'description', content: descr },
+        { hid: 'og:description', name: 'og:description', content: descr },
+        { hid: 'og:type', name: 'og:type', content: type }
+      ]
+    }
+  }
 }
 </script>
 
--- blog/pages/blog/_id/index.vue
+++ blog/pages/blog/_id/index.vue
@@ -18,24 +18,29 @@ import comments from '@/components/Comments/Comments.vue'
 export default {
   name: "index",
   components: { post, newComment, comments },
+  head () {
+    let title = this.post.title,
+        descr = this.post.descr,
+        img = `${this.post.img}`,
+        // img = `site.com/${this.post.img}`,
+        type = 'article'
+
+    return {
+      title: title,
+      meta: [
+        { hid: 'og:title', name: 'og:title', content: title },
+        { hid: 'description', name: 'description', content: descr },
+        { hid: 'og:description', name: 'og:description', content: descr },
+        { hid: 'og:type', name: 'og:type', content: type },
+        { hid: 'og:img', name: 'og:img', content: img }
+      ]
+    }
+  },
   async asyncData (context) {
     let [post, comments] = await Promise.all([
       axios.get(`https://blog-nuxt-f5235-default-rtdb.europe-west1.firebasedatabase.app/posts/${context.params.id}.json`),
       axios.get(`https://blog-nuxt-f5235-default-rtdb.europe-west1.firebasedatabase.app/comments.json`)
     ])
-
-    // let commentsArray = [],
-    //   commentsArrayRes = []
-    //
-    // Object.keys(comments.data).forEach(key => {
-    //   commentsArray.push(comments.data[key])
-    // })
-    //
-    // for (let i = 0; i < commentsArray.length; i++) {
-    //   if (commentsArray[i].postId === context.params.id && commentsArray[i].publish === true) {
-    //     commentsArrayRes.push(commentsArray[i])
-    //   }
-    // }
 
     let commentsArrayRes = Object.values(comments.data).filter(comment => (comment.postId === context.params.id) && comment.publish)
 
